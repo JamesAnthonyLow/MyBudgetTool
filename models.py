af80ffcf9fee368dc5935d0ff3de4e0bbd828510
JamesAnthonyLow <jamesalowenthal@gmail.com>
2023-02-26 21:43:52 -0500

    wip

--- models.py
+++ models.py
@@ -6,14 +6,14 @@ import typing
 import pydantic
 
 
-class ChaseBankAccountDetails(enum.Enum):
+class ChaseBankAccountActivityDetails(enum.Enum):
     CHECK = "CHECK"
     CREDIT = "CREDIT"
     DEBIT = "DEBIT"
     DSLIP = "DSLIP"
 
 
-class ChaseBankAccountType(enum.Enum):
+class ChaseBankAccountActivityType(enum.Enum):
     ACCT_XFER = "ACCT_XFER"
     ACH_CREDIT = "ACH_CREDIT"
     ACH_DEBIT = "ACH_DEBIT"
@@ -31,28 +31,35 @@ class ChaseBankAccountType(enum.Enum):
     REFUND_TRANSACTION = "REFUND_TRANSACTION"
 
 
-class ChaseBankAccount(pydantic.BaseModel):
-    details: ChaseBankAccountDetails
-    posting_date: datetime.date
+class ChaseDate(datetime.date):
+    @classmethod
+    def __get_validators__(cls):
+        def _parse_date(value) -> datetime.date:
+            if isinstance(value, str):
+                return datetime.datetime.strptime(value, "%m/%d/%Y").date()
+            else:
+                return value
+
+        yield _parse_date
+
+
+class ChaseBankAccountActivity(pydantic.BaseModel):
+    details: ChaseBankAccountActivityDetails
+    posting_date: ChaseDate
     description: str
     amount: float
-    type: ChaseBankAccountType
+    type: ChaseBankAccountActivityType
     balance: float
     check_or_slip_no: typing.Optional[str]
 
-    @pydantic.validator("posting_date", pre=True)
-    def parse_birthdate(cls, value: typing.Union[str, datetime.date]) -> datetime.date:
-        if isinstance(value, str):
-            return datetime.datetime.strptime(value, "%m/%d/%Y").date()
-        else:
-            return value
-
     @classmethod
-    def from_csv(cls, filename: str) -> typing.List["ChaseBankAccount"]:
+    def from_csv(cls, filename: str) -> typing.List["ChaseBankAccountActivity"]:
         with open(filename) as csvfile:
             reader = csv.DictReader(csvfile)
 
-            def _from_row(row: typing.Dict[str, typing.Any]) -> "ChaseBankAccount":
+            def _from_row(
+                row: typing.Dict[str, typing.Any]
+            ) -> "ChaseBankAccountActivity":
                 try:
                     return cls(
                         details=row["Details"],
@@ -61,10 +68,31 @@ class ChaseBankAccount(pydantic.BaseModel):
                         amount=row["Amount"],
                         type=row["Type"],
                         balance=row["Balance"],
-                        check_or_slip_no=row["Check or Slip #"],
+                        check_or_slip_no=row["Check or Slip #"]
+                        if row["Check or Slip #"] != ""
+                        else None,
                     )
                 except pydantic.ValidationError as e:
                     e.add_note(f"{row}")
                     raise e
 
             return [_from_row(row) for row in reader]
+
+
+class ChaseCreditCardActivity(pydantic.BaseModel):
+    transaction_date: ChaseDate
+    post_date: ChaseDate
+    description: str
+    category: str
+    type: str
+    amount: float
+    memo: typing.Optional[str]
+
+    @pydantic.validator("transaction_date", pre=True)
+    def parse_transaction_date(
+        cls, value: typing.Union[str, datetime.date]
+    ) -> datetime.date:
+        if isinstance(value, str):
+            return datetime.datetime.strptime(value, "%m/%d/%Y").date()
+        else:
+            return value
--- tests/test_chase_bank_account.py
+++ tests/test_chase_bank_account.py
@@ -1,55 +1,57 @@
-import pytest
-
 import datetime
-from models import ChaseBankAccount, ChaseBankAccountDetails, ChaseBankAccountType
+from models import (
+    ChaseBankAccountActivity,
+    ChaseBankAccountActivityDetails,
+    ChaseBankAccountActivityType,
+)
 
 
 def test_parse_csv():
-    rows = ChaseBankAccount.from_csv("tests/chase_bank_account.csv")
+    rows = ChaseBankAccountActivity.from_csv("tests/chase_bank_account.csv")
     assert rows == [
-        ChaseBankAccount(
-            details=ChaseBankAccountDetails.DEBIT,
+        ChaseBankAccountActivity(
+            details=ChaseBankAccountActivityDetails.DEBIT,
             posting_date=datetime.date(2023, 2, 22),
             description="Hello World1",
             amount=-600.0,
-            type=ChaseBankAccountType.ACH_DEBIT,
+            type=ChaseBankAccountActivityType.ACH_DEBIT,
             balance=11806.0,
-            check_or_slip_no="",
+            check_or_slip_no=None,
         ),
-        ChaseBankAccount(
-            details=ChaseBankAccountDetails.DEBIT,
+        ChaseBankAccountActivity(
+            details=ChaseBankAccountActivityDetails.DEBIT,
             posting_date=datetime.date(2023, 2, 21),
             description="Hello World2",
             amount=-61.19,
-            type=ChaseBankAccountType.ACH_DEBIT,
+            type=ChaseBankAccountActivityType.ACH_DEBIT,
             balance=12406.0,
-            check_or_slip_no="",
+            check_or_slip_no=None,
         ),
-        ChaseBankAccount(
-            details=ChaseBankAccountDetails.DEBIT,
+        ChaseBankAccountActivity(
+            details=ChaseBankAccountActivityDetails.DEBIT,
             posting_date=datetime.date(2023, 2, 15),
             description="Hello World3",
             amount=-438.71,
-            type=ChaseBankAccountType.ACH_DEBIT,
+            type=ChaseBankAccountActivityType.ACH_DEBIT,
             balance=12667.19,
-            check_or_slip_no="",
+            check_or_slip_no=None,
         ),
-        ChaseBankAccount(
-            details=ChaseBankAccountDetails.DEBIT,
+        ChaseBankAccountActivity(
+            details=ChaseBankAccountActivityDetails.DEBIT,
             posting_date=datetime.date(2023, 2, 15),
             description="Hello World4",
             amount=-600.0,
-            type=ChaseBankAccountType.ACH_DEBIT,
+            type=ChaseBankAccountActivityType.ACH_DEBIT,
             balance=13105.9,
-            check_or_slip_no="",
+            check_or_slip_no=None,
         ),
-        ChaseBankAccount(
-            details=ChaseBankAccountDetails.DEBIT,
+        ChaseBankAccountActivity(
+            details=ChaseBankAccountActivityDetails.DEBIT,
             posting_date=datetime.date(2023, 2, 2),
             description="Hello World5",
             amount=-1096.18,
-            type=ChaseBankAccountType.ACH_DEBIT,
+            type=ChaseBankAccountActivityType.ACH_DEBIT,
             balance=20602.22,
-            check_or_slip_no="",
+            check_or_slip_no=None,
         ),
     ]
